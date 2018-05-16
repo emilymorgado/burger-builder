111d2fe83627159b8f017aa6bc922460a0fa74cb
Emily Morgado <emilyannemorgado@gmail.com>
2018-05-15 20:25:51 -0700

    Add build controls to the burger and ensure functionality

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,8 @@ import React, { Component } from 'react';
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
+import Modal from '../../components/UI/Modal/Modal';
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -20,6 +22,19 @@ class BurgerBuilder extends Component {
       meat: 0,
     },
     totalPrice: 4,
+    purchaseable: false,
+    purchasing: false,
+  }
+
+  updatePurchaseState (ingredients) {
+    const sum = Object.keys(ingredients)
+      .map(ingredKey => {
+        return ingredients[ingredKey];
+    })
+    .reduce((sum, elem) => {
+      return sum + elem;
+    }, 0);
+    this.setState({purchaseable: sum > 0})
   }
 
   addIngredientHandler = (type) => {
@@ -33,6 +48,7 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice + priceAddition;
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+    this.updatePurchaseState(updatedIngredients);
   }
 
   removeIngredientHandler = (type) => {
@@ -50,6 +66,19 @@ class BurgerBuilder extends Component {
     const oldPrice = this.state.totalPrice;
     const newPrice = oldPrice - priceDeduction;
     this.setState({ totalPrice: newPrice, ingredients: updatedIngredients });
+    this.updatePurchaseState(updatedIngredients);
+  }
+
+// 'this' syntax doesn't work as expected when triggered through an event
+// When triggered by an event, it doesn't refer to the Class
+// add/remove ingredients funtions use arrow functions, which preserve
+// 'this' scoping
+// Thus, we change this to the arrow function below
+  // purchaseHandler () {
+  //   this.setState({purchasing: true});
+  // }
+  purchaseHandler = () => {
+    this.setState({purchasing: true});
   }
 
   render () {
@@ -61,11 +90,17 @@ class BurgerBuilder extends Component {
     }
     return (
       <Aux>
+        <Modal show={this.state.purchasing}>
+          <OrderSummary ingredients={this.state.ingredients} />
+        </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
           ingredientAdded={this.addIngredientHandler}
           ingredientRemoved={this.removeIngredientHandler}
-          disabled={disabledInfo} />
+          disabled={disabledInfo}
+          purchaseable={this.state.purchaseable}
+          ordered={this.purchaseHandler}
+          price={this.state.totalPrice} />
       </Aux>
     );
   }
