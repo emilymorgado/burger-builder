ce094eb40b93d8b594a261d9e02bac93e2e54709
Emily Morgado <emilyannemorgado@gmail.com>
2018-05-17 20:21:48 -0700

    Add logo and navigation

---
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import classes from './NavigationItems.css';
+import NavigationItem from './NavigationItem/NavigationItem';
+
+// Don't have to set active={true} because bool items are implied
+const navigationItems = () => (
+  <ul className={classes.NavigationItems}>
+    <NavigationItem link="/" active>Burger Builder</NavigationItem>
+    <NavigationItem link="/">Checkout</NavigationItem>
+  </ul>
+);
+
+export default navigationItems;
